ab942912f679c83f24b1d487bdac9d4bb4d897fa
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-02-08 20:56:11 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/project-drink-it">
+        <BrowserRouter basename="/fs-87-react-node-barinio">
           <Normalize />
           <App />
         </BrowserRouter>
